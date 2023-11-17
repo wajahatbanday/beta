c1511d5657b4c815ff4f227c169ea801352904c0
Wajahat Banday <dev@wajahat.io>
2023-11-17 14:59:26 +0530

    Tailwind Testing Complete

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,13 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/card";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="bg-green-400 text-8xl">Tailwind Test</h1>
+      <div className="">
+        <div className="flex justify-center">
+          <h1 className="text-white p-4 rounded-xl mb-4 w-[400px]">
+            Tailwind Test
+          </h1>
+        </div>
+        <div className="flex gap-[10px]">
+          <Card username="Wajahat" age="28" />
+          <Card username="Iqram" age="26" />
+          <Card username="Suhaib" age="28" />
+        </div>
+      </div>
     </>
   );
 }
