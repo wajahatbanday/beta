57b840f77d7ab2601a777aa87334ea3f729e45aa
Wajahat Banday <dev@wajahat.io>
2023-11-18 01:41:40 +0530

    Improved UI

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -24,39 +24,40 @@ function App() {
   }, [length, numberAllowed, charatersAllowed, passwordGenerator]);
   return (
     <>
-      <div className="flex justify-center w-[100vw] border-2 border-red-500">
-        <div className="w-full max-w-md mx-auto shadow-md rounded-lg p-5 my-8 text-black bg-blue-300  border-2 border-red-500">
-          <h1 className="text-white text-center text-[25px] p-2">
+      <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-gradient-to-r from-blue-600 to-blue-300">
+        <div className="w-full max-w-md shadow-xl rounded-lg p-5 my-8 text-black bg-gradient-to-r from-blue-300 to-blue-600">
+          <h1 className="text-black text-center text-[25px] p-2">
             Password Generator
           </h1>
           <div className="flex shadow rounded-lg overflow-hidden mb-4">
             <input
               type="text"
               value={password}
-              className="outline-none w-full py-1 px-3"
+              className="outline-none w-full py-1 px-3 text-center"
               placeholder="password"
               readOnly
             />
-            <button className="outline-none bg-blue-800 text-white px-3 py-0.5 shrink-0">
+            <button className="outline-none bg-blue-300 text-black px-3 py-0.5 shrink-0">
               Copy
             </button>
           </div>
-          <div className="flex text-sm gap-x-2">
+          <div className="flex justify-center text-sm gap-x-5">
             <div className="flex items-center gap-x-1">
               <input
                 type="range"
-                min={6}
+                min={8}
                 max={100}
                 value={length}
-                className="cursor-pointer"
+                className="cursor-pointer accent-blue-800"
                 onChange={(e) => {
                   setLength(e.target.value);
                 }}
               />
-              <label>Length: {length}</label>
+              <label>{length}</label>
             </div>
             <div className="flex items-center gap-x-1">
               <input
+                className="accent-blue-800"
                 type="checkbox"
                 defaultChecked={numberAllowed}
                 id="numberInput"
@@ -64,10 +65,11 @@ function App() {
                   setNumberAllowed((prev) => !prev);
                 }}
               />
-              <label htmlFor="numberInput"> Numbers</label>
+              <label htmlFor="numberInput">Numbers</label>
             </div>
             <div className="flex items-center gap-x-1">
               <input
+                className="accent-blue-800"
                 type="checkbox"
                 defaultChecked={charatersAllowed}
                 id="characterInput"
