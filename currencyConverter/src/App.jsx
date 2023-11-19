09139fdb1ed39f6c91ef507cd3a680a45880b34f
Wajahat Banday <dev@wajahat.io>
2023-11-19 22:53:57 +0530

    Currency Converter - Built useCurrencyInfo Hook

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import { InputBox } from "./components";
+import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [amout, setAmount] = useState(0);
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("inr");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const currencyInfo = useCurrencyInfo(from);
 
   return (
     <>
