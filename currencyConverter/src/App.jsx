47e9fd6dcee04a1895fdd9b63e7259e5c51dc6d6
Wajahat Banday <dev@wajahat.io>
2023-11-20 15:16:51 +0530

    Currency Converter Completed ✅

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -1,20 +1,81 @@
 import { useState } from "react";
-import "./App.css";
 import { InputBox } from "./components";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 
 function App() {
-  const [amout, setAmount] = useState(0);
+  const [amount, setAmount] = useState(0);
   const [from, setFrom] = useState("usd");
   const [to, setTo] = useState("inr");
   const [convertedAmount, setConvertedAmount] = useState(0);
   const currencyInfo = useCurrencyInfo(from);
 
+  const options = Object.keys(currencyInfo);
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+    setConvertedAmount(amount);
+    setAmount(convertedAmount);
+  };
+
+  const convert = () => {
+    setConvertedAmount(amount * currencyInfo[to]);
+  };
+
   return (
-    <>
-      <h1 className="text-3xl bg-blue-300">Currency App</h1>
-    </>
+    <div
+      className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{
+        backgroundImage: `url('https://images.pexels.com/photos/259209/pexels-photo-259209.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')`,
+      }}
+    >
+      <div className="w-full">
+        <div className="flex flex-col justify-center items-center w-full max-w-md mx-auto rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          <form
+            className="w-full flex flex-col gap-1"
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <div className="w-full">
+              <InputBox
+                label="From"
+                amount={amount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setAmount(amount)}
+                selectCurrency={from}
+                onAmountChange={(amount) => setAmount(amount)}
+              />
+            </div>
+            <div className="flex justify-center items-center w-full h-10 gap-[3rem]">
+              <button
+                type="submit"
+                className="bg-gradient-to-r from-gray-600 to-gray-300 text-white px-2 py-0.5 rounded-lg w-20 hover:scale-125"
+              >
+                Convert
+              </button>
+              <button
+                type="button"
+                className="rounded-md bg-gradient-to-r from-gray-300 to-gray-600 text-white px-2 py-0.5 w-20 hover:scale-125"
+                onClick={swap}
+              >
+                Swap
+              </button>
+            </div>
+            <div className="w-full">
+              <InputBox
+                label="To"
+                amount={convertedAmount}
+                currencyOptions={options}
+                onCurrencyChange={(currency) => setTo(currency)}
+                selectCurrency={to}
+                amountDisable
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default App;
--- currencyConverter/src/components/InputBox.jsx
+++ currencyConverter/src/components/InputBox.jsx
@@ -16,12 +16,12 @@ function InputBox({
   return (
     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
       <div className="w-1/2">
-        <label
+        {/* <label
           htmlFor={amountInputId}
           className="text-black/40 mb-2 inline-block"
         >
           {label}
-        </label>
+        </label> */}
         <input
           id={amountInputId}
           className="outline-none w-full bg-transparent py-1.5"
@@ -35,7 +35,7 @@ function InputBox({
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
-        <p className="text-black/40 mb-2 w-full">Currency</p>
+        {/* <p className="text-black/40 mb-2 w-full">Currency</p> */}
         <select
           className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectCurrency}
